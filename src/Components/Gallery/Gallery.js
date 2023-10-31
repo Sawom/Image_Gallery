5a48cb649d428662850bbffe73a1aa5cd39d66bf
Sawom <asawom250@gmail.com>
2023-10-31 22:48:03 +0600

    setup done

---
+++ src/Components/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='container mx-auto'>
+            eta ekta gallery
+        </div>
+    );
+};
+
+export default Gallery;
